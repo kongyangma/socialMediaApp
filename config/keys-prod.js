6287cc3fc3b37d1386de31113c896ba109cd77e8
kongyang ma <kongyangma@hotmail.com>
2019-10-21 16:17:08 -0700

    deployment

---
+++ config/keys-prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://socialmediaapp:socialmediaapp1@socialmediaapp-5rzfh.mongodb.net/test?retryWrites=true&w=majority',
+    GoogleClientID: '515753399709-fus51c1h007nkendcug2b8nu53i8i169.apps.googleusercontent.com',
+    GoogleClientSecret: 'bHT8VMMLkGuQMpEX99Gqrx8h'
+}
