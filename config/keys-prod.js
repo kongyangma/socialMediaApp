6f4fda3712cd0cc2b5dbfd404c33e2d32ba24bce
kongyang ma <kongyangma@hotmail.com>
2019-10-21 17:40:39 -0700

    config keys added

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://socialmediaapp:socialmediaapp1@socialmediaapp-5rzfh.mongodb.net/test?retryWrites=true&w=majority',
-    GoogleClientID: '515753399709-fus51c1h007nkendcug2b8nu53i8i169.apps.googleusercontent.com',
-    GoogleClientSecret: 'bHT8VMMLkGuQMpEX99Gqrx8h'
+    MongoURI: process.env.MONGO_URI,
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
